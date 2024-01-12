acf2087f2cf680362098d34fece1cee0b45d320e
babaquero07 <brayanalexanderb@gmail.com>
2024-01-12 16:46:03 -0500

    Add product grid to shop page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,15 @@
 import Title from "@/components/ui/title/Title";
+import { initialData } from "@/seed/seed";
+import ProductGrid from "@/components/products/product-grid/product-grid";
+
+const products = initialData.products;
 
 const Home = () => {
   return (
     <>
       <Title title="Shop" subtitle="All products" className="mb-2" />
+
+      <ProductGrid products={products} />
     </>
   );
 };
