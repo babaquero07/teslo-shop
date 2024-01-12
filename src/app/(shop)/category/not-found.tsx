61567df5060055ee779296301ce21db1858a7647
babaquero07 <brayanalexanderb@gmail.com>
2024-01-12 16:10:09 -0500

    Refactor 404 Not Found page to use PageNotFound component

--- src/app/(shop)/category/not-found.tsx
+++ src/app/(shop)/category/not-found.tsx
@@ -1,11 +1,5 @@
-import Link from "next/link";
+import PageNotFound from "@/components/ui/not-found/PageNotFound";
 
 export default function () {
-  return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  );
+  return <PageNotFound />;
 }
