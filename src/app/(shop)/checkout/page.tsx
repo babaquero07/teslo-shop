66fe9a4885f30f205d15d9d5a1212fefd8eccaca
babaquero07 <brayanalexanderb@gmail.com>
2024-01-15 17:31:09 -0500

    Refactor checkout page layout and add order summary

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,96 @@
+import Link from "next/link";
+
+import Title from "@/components/ui/title/Title";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import QuantitySelector from "@/components/product/quantity-selector/QuantitySelector";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function () {
   return (
-    <div>
-      <h1>Checkout page</h1>
+    <div className="flex justify-center items center mb-72 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verify order" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Edit order</span>
+            <Link href="/cart" className="underline mb-5">
+              Edit cart
+            </Link>
+
+            {/* Items */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  className="mr-5 rounded"
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  alt={product.title}
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} x 3</p>
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Checkout */}
+          <div className="bg-white rounded-xl p-7">
+            <h2 className="text-2xl font-bold mb-2">Shipping address</h2>
+            <div className="mb-10">
+              <p className="text-xl">Alexander Baquero</p>
+              <p>Av. Siempre viva 123</p>
+              <p>USA</p>
+            </div>
+
+            {/* Divider */}
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+
+            <h2 className="text-2xl font-bold mb-2">Order summary</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Products</span>
+              <span className="text-right">3 products</span>
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+              <span>Taxes (15 %)</span>
+              <span className="text-right">$ 100</span>
+              <span className="text-2xl mt-5">Total</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                {/* Disclaimer */}
+                <span className="text-xs">
+                  By placing your order, you agree to our terms of service and
+                  privacy policy.
+                </span>
+              </p>
+              <Link
+                className="flex btn-primary justify-center"
+                href="/orders/123"
+              >
+                Next
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
