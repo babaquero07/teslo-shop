cc0ed4da473cdd3203d9e007388eb1a7c29fe1c6
babaquero07 <brayanalexanderb@gmail.com>
2024-01-15 16:17:31 -0500

    Update className prop in Title component

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -2,7 +2,7 @@ import { titleFont } from "@/config/fonts";
 interface Props {
   title: string;
   subtitle?: string;
-  className: string;
+  className?: string;
 }
 
 const Title = ({ title, subtitle, className }: Props) => {
