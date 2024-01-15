1012dbf77cacbf1180b820d7156ee2d75ebd35e2
babaquero07 <brayanalexanderb@gmail.com>
2024-01-15 17:04:23 -0500

    Refactor cart page layout and add quantity selector

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,81 @@
+import Link from "next/link";
+
+import Title from "@/components/ui/title/Title";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import QuantitySelector from "@/components/product/quantity-selector/QuantitySelector";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function () {
   return (
-    <div>
-      <h1>Cart page</h1>
+    <div className="flex justify-center items center mb-72 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Cart" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Add more items</span>
+            <Link href="/" className="underline mb-5">
+              Continue shopping
+            </Link>
+
+            {/* Items */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  className="mr-5 rounded"
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  alt={product.title}
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={2} />
+
+                  <button className="underline mt-3">Remover</button>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Checkout */}
+          <div className="bg-white rounded-xl p-7">
+            <h2 className="text-2xl mb-2">Order summary</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Products</span>
+              <span className="text-right">3 products</span>
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+              <span>Taxes (15 %)</span>
+              <span className="text-right">$ 100</span>
+              <span className="text-2xl mt-5">Total</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full align-bottom">
+              <Link
+                className="flex btn-primary justify-center"
+                href="/checkout/address"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
