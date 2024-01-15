84c4c5272324c202a458af3268579f451895cfba
babaquero07 <brayanalexanderb@gmail.com>
2024-01-15 17:21:34 -0500

    Refactor cart and checkout address pages

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,7 +1,66 @@
+import Link from "next/link";
+import Title from "@/components/ui/title/Title";
+
 export default function () {
   return (
-    <div>
-      <h1>Address page</h1>
+    <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
+      <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
+        <Title title="Address" subtitle="Shipping address" />
+
+        <div className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
+          <div className="flex flex-col mb-2">
+            <span>Name</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Last name</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Address</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Address 2 (optional)</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>ZIP code</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>City</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Country</span>
+            <select className="p-2 border rounded-md bg-gray-200">
+              <option value="">[ Seleccione ]</option>
+              <option value="CO">Colombia</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Phone number</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2 sm:mt-10">
+            <Link
+              href="/checkout"
+              className="btn-primary flex w-full sm:w-1/2 justify-center "
+            >
+              Next
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
