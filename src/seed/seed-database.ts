08c54f20259626c4db2d6c4fea02a1062e932e1e
babaquero07 <brayanalexanderb@gmail.com>
2024-01-16 16:05:11 -0500

    Add Prisma client and seed database

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,6 +1,15 @@
+import prisma from "../lib/prisma";
+
 import { initialData } from "./seed";
 
 async function main() {
+  // Delete all data
+  await Promise.all([
+    await prisma.productImage.deleteMany(),
+    await prisma.product.deleteMany(),
+    await prisma.category.deleteMany(),
+  ]);
+
   console.log("Seed executed!");
 }
 
