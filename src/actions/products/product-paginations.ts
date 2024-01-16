b66ae296557ae5f80253e3087ee957e5b9a6151e
babaquero07 <brayanalexanderb@gmail.com>
2024-01-16 17:35:50 -0500

    show products

--- src/actions/products/product-paginations.ts
+++ src/actions/products/product-paginations.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "../../lib/prisma";
+import prisma from "@/lib/prisma";
 
 export const getPaginatedProductsWithImages = async (
   page: number,
@@ -17,8 +17,14 @@ export const getPaginatedProductsWithImages = async (
         },
       },
     });
-    console.log("🚀 ~ products:", products);
+
+    return {
+      products: products.map((product) => ({
+        ...product,
+        images: product.ProductImage.map((image) => image.url),
+      })),
+    };
   } catch (error) {
-    console.log(error);
+    throw new Error("Error getting products with images");
   }
 };
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,10 @@
 import Title from "@/components/ui/title/Title";
-import { initialData } from "@/seed/seed";
 import ProductGrid from "@/components/products/product-grid/product-grid";
+
 import { getPaginatedProductsWithImages } from "@/actions/products/product-paginations";
 
-const products = initialData.products;
-
 const Home = async () => {
-  const productsTemp = await getPaginatedProductsWithImages(1, 3);
+  const { products } = await getPaginatedProductsWithImages(1, 3);
 
   return (
     <>
