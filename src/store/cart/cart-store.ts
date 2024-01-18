461027a19e1decd76923c671f405d55c9af43bce
babaquero07 <brayanalexanderb@gmail.com>
2024-01-18 15:12:06 -0500

    Add cart persistence to useCartStore

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -7,30 +7,38 @@ interface State {
   addProductToCart: (product: CartProduct) => void;
 }
 
-export const useCartStore = create<State>()((set, get) => ({
-  cart: [],
-  addProductToCart: (product: CartProduct) => {
-    const { cart } = get();
+export const useCartStore = create<State>()(
+  // Persist the cart in local storage
+  persist(
+    (set, get) => ({
+      cart: [],
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
 
-    // Check if product is already in cart with the size selected
-    const productInCart = cart.some(
-      (item) => item.id === product.id && item.size === product.size
-    );
+        // Check if product is already in cart with the size selected
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
 
-    if (!productInCart) {
-      set({ cart: [...cart, product] });
-      return;
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
+        }
+
+        // If product is already in cart, update the quantity
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+
+          return item;
+        });
+
+        set({ cart: updatedCartProducts });
+      },
+    }),
+    {
+      name: "shopping-cart",
     }
-
-    // If product is already in cart, update the quantity
-    const updatedCartProducts = cart.map((item) => {
-      if (item.id === product.id && item.size === product.size) {
-        return { ...item, quantity: item.quantity + product.quantity };
-      }
-
-      return item;
-    });
-
-    set({ cart: updatedCartProducts });
-  },
-}));
+  )
+);
