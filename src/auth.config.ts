000090e6fc5b57667861c86a368185a45aea1e89
babaquero07 <brayanalexanderb@gmail.com>
2024-01-19 15:24:22 -0500

    Add zod package and implement credentials authorization

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,29 @@
-import type { NextAuthConfig } from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import { z } from "zod";
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/new-account",
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+        console.log(email, password);
+
+        return null;
+      },
+    }),
+  ],
 };
+
+export const { signIn, signOut, auth: middleware } = NextAuth(authConfig);
