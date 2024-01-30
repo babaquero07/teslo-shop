dec0ba14eed46f16f74b97c24c654622cc7b8f45
babaquero07 <brayanalexanderb@gmail.com>
2024-01-30 16:05:26 -0500

    Add ProductsInCart and PlaceOrder components

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 
 import Title from "@/components/ui/title/Title";
+
 import ProductsInCart from "./ui/ProductsInCart";
+import PlaceOrder from "./ui/PlaceOrder";
 
 export default function CheckoutPage() {
   return (
@@ -22,45 +24,7 @@ export default function CheckoutPage() {
           </div>
 
           {/* Checkout */}
-          <div className="bg-white rounded-xl p-7">
-            <h2 className="text-2xl font-bold mb-2">Shipping address</h2>
-            <div className="mb-10">
-              <p className="text-xl">Alexander Baquero</p>
-              <p>Av. Siempre viva 123</p>
-              <p>USA</p>
-            </div>
-
-            {/* Divider */}
-            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
-
-            <h2 className="text-2xl font-bold mb-2">Order summary</h2>
-            <div className="grid grid-cols-2">
-              <span>No. Products</span>
-              <span className="text-right">3 products</span>
-              <span>Subtotal</span>
-              <span className="text-right">$ 100</span>
-              <span>Taxes (15 %)</span>
-              <span className="text-right">$ 100</span>
-              <span className="text-2xl mt-5">Total</span>
-              <span className="mt-5 text-2xl text-right">$ 100</span>
-            </div>
-
-            <div className="mt-5 mb-2 w-full">
-              <p className="mb-5">
-                {/* Disclaimer */}
-                <span className="text-xs">
-                  By placing your order, you agree to our terms of service and
-                  privacy policy.
-                </span>
-              </p>
-              <Link
-                className="flex btn-primary justify-center"
-                href="/orders/123"
-              >
-                Next
-              </Link>
-            </div>
-          </div>
+          <PlaceOrder />
         </div>
       </div>
     </div>
---
+++ src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import { useCartStore, useAddressStore } from "@/store";
+
+import { currencyFormat } from "@/utils/currencyFormat";
+
+const PlaceOrder = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  const address = useAddressStore((state) => state.address);
+
+  const { totalItems, subTotal, taxes, total } = useCartStore((state) =>
+    state.getOrderSummary()
+  );
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="bg-white rounded-xl p-7">
+      <h2 className="text-2xl font-bold mb-2">Shipping address</h2>
+      <div className="mb-10">
+        <p className="text-xl">
+          {address.firstName} {address.lastName}
+        </p>
+        <p>{address.address}</p>
+        <p>{address.address2}</p>
+        <p>{address.zipCode}</p>
+        <p>
+          {address.city}, {address.country}
+        </p>
+        <p>{address.phone}</p>
+      </div>
+
+      {/* Divider */}
+      <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+
+      <h2 className="text-2xl font-bold mb-2">Order summary</h2>
+      <div className="grid grid-cols-2">
+        <span>No. Products</span>
+        <span className="text-right">
+          {totalItems === 1 ? "1 product" : `${totalItems} products`}
+        </span>
+        <span>Subtotal</span>
+        <span className="text-right">{currencyFormat(subTotal)}</span>
+        <span>Taxes (15 %)</span>
+        <span className="text-right">{currencyFormat(taxes)}</span>
+        <span className="text-2xl mt-5">Total</span>
+        <span className="mt-5 text-2xl text-right">
+          {currencyFormat(total)}
+        </span>
+      </div>
+
+      <div className="mt-5 mb-2 w-full">
+        <p className="mb-5">
+          {/* Disclaimer */}
+          <span className="text-xs">
+            By placing your order, you agree to our terms of service and privacy
+            policy.
+          </span>
+        </p>
+        <button className="flex btn-primary justify-center">Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceOrder;
