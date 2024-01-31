128e9b0c4f82d8461f20c94d0b700900b0b5d72c
babaquero07 <brayanalexanderb@gmail.com>
2024-01-31 13:49:58 -0500

    Add product information and calculate total price in placeOrder function

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -1,8 +1,8 @@
 "use server";
 
 import type { Address, Size } from "@/interfaces";
-
 import { auth } from "@/auth.config";
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
   productId: string;
@@ -23,4 +23,37 @@ export const placeOrder = async (
       message: "There is no user logged in.",
     };
   }
+
+  // Get products information
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((product) => product.productId),
+      },
+    },
+  });
+
+  // Calculate total price
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+
+  // Subtotal, tax and total
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((p) => p.id === item.productId);
+
+      if (!product)
+        throw new Error("Product not found with id: " + item.productId);
+
+      const subTotal = product.price * productQuantity;
+      totals.subTotal += subTotal;
+      totals.tax += subTotal * 0.15;
+      totals.total += subTotal * 1.15;
+
+      return totals;
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  );
+
+  // Create the order
 };
